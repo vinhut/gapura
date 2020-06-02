82afb4c038b9c900194b9b56d03ad7c41dc76b94
vinhut <hutama.alvin@gmail.com>
2020-06-02 14:08:04 +0000

    Add login test

--- go.mod
+++ go.mod
@@ -3,7 +3,7 @@ module github.com/vinhut/gapura
 require (
 	github.com/gin-contrib/sessions v0.0.3 // indirect
 	github.com/gin-gonic/gin v1.5.0
-	github.com/golang/mock v1.4.3
+	github.com/golang/mock v1.4.0
 	github.com/stretchr/testify v1.4.0
 	github.com/utrack/gin-csrf v0.0.0-20190424104817-40fb8d2c8fca // indirect
 	go.mongodb.org/mongo-driver v1.3.0
--- main_test.go
+++ main_test.go
@@ -4,9 +4,14 @@ import (
 	"github.com/golang/mock/gomock"
 	"github.com/stretchr/testify/assert"
 	mocks "github.com/vinhut/gapura/mocks"
+	"github.com/vinhut/gapura/models"
 
+	"bytes"
+	"fmt"
 	"net/http"
 	"net/http/httptest"
+	"net/url"
+	"os"
 	"testing"
 )
 
@@ -24,4 +29,38 @@ func TestPing(t *testing.T) {
 
 	assert.Equal(t, 200, w.Code)
 	assert.Equal(t, "OK", w.Body.String())
+}
+
+func TestLogin(t *testing.T) {
+
+	test_email := "newusertest@test.com"
+	test_password := "test_password"
+	hashed_password := "$2a$14$quU8rC8Cfska91KtagAkhOPdMvQ5sEPMwelBFDFvrdUR2/uCoa/MC"
+
+	os.Setenv("KEY", "12345678901234567890123456789012")
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+	mock_user := mocks.NewMockUserDatabase(ctrl)
+	input := models.User{}
+	mock_user.EXPECT().Find("email", test_email, &input).Do(
+		func(a string, b string, c *models.User) error {
+			c.Email = test_email
+			c.Password = hashed_password
+			c.Role = "standard"
+			return nil
+		})
+
+	router := setupRouter(mock_user)
+
+	var param = url.Values{}
+	param.Set("email", test_email)
+	param.Set("password", test_password)
+	var payload = bytes.NewBufferString(param.Encode())
+
+	pre_w := httptest.NewRecorder()
+	pre_req, _ := http.NewRequest("POST", "/login", payload)
+	pre_req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
+	router.ServeHTTP(pre_w, pre_req)
+
+	assert.Equal(t, 200, pre_w.Code)
 }
