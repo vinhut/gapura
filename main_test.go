dac34cfac2bb778bf6c26cdb38ed1d184499b6a4
vinhut <hutama.alvin@gmail.com>
2020-06-04 13:15:08 +0000

    Test decrypt token in cookies

--- main_test.go
+++ main_test.go
@@ -84,6 +84,26 @@ func TestDecryptToken(t *testing.T) {
 	assert.Equal(t, 200, rec.Code)
 }
 
+func TestDecryptTokenFromCookies(t *testing.T) {
+
+	service_name := "test-service"
+	token := "852a37a34b727c0e0b331806-7af4bdfdcc60990d427f383efecc8529289d040dd67e0753b9e2ee5a1e938402186f28324df23f6faa4e2bbf43f584ae228c55b00143866215d6e92805d470a1cc2a096dcca4d43527598122313be412e17fbefdcdab2fae02e06a405791d936862d4fba688b3c7fd784d4"
+
+	os.Setenv("KEY", "12345678901234567890123456789012")
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+	mock_user := mocks.NewMockUserDatabase(ctrl)
+
+	router := setupRouter(mock_user)
+
+	rec := httptest.NewRecorder()
+	req, _ := http.NewRequest("GET", "/user?service="+service_name, nil)
+	req.Header.Set("Cookie", "token="+token+";")
+	router.ServeHTTP(rec, req)
+
+	assert.Equal(t, 200, rec.Code)
+}
+
 func TestPublicEndpoint(t *testing.T) {
 
 	ctrl := gomock.NewController(t)
