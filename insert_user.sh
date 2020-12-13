8acac111829ca42dc1c47f1592fc0d7f133d9f9d
vinhut <hutama.alvin@gmail.com>
2020-12-13 17:15:24 +0000

    add second index for _id in pamulangdb

--- insert_user.sh
+++ insert_user.sh
@@ -6,4 +6,4 @@ do
 	curl -XPOST -d "email=$email&password=$pass&username=$username" $1/user?service=internal
 done < username.txt 
 
-kubectl -n auth-test exec -it mongodb-auth-0 -- mongo --quiet -umongoadmin -psecret --authenticationDatabase "admin" --eval "db.users.createIndex({username:1,email:1})" pamulangdb
+kubectl -n auth-test exec -it mongodb-auth-0 -- mongo --quiet -umongoadmin -psecret --authenticationDatabase "admin" --eval "db.users.createIndex({_id:1,username:1,email:1})" pamulangdb
