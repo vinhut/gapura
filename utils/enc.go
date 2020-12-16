f068e52b4b591ddc5466fac29776090e314a3dd7
vinhut <hutama.alvin@gmail.com>
2020-12-16 09:05:04 +0000

    Replace bcrypt to pbkdf2 for speed

--- utils/enc.go
+++ utils/enc.go
@@ -1,9 +1,15 @@
 package utils
 
 import (
+	"bytes"
 	"crypto/aes"
 	"crypto/cipher"
+	"crypto/rand"
+	"crypto/sha256"
 	"encoding/hex"
+	"errors"
+	"golang.org/x/crypto/pbkdf2"
+	"strings"
 )
 
 func GCM_encrypt(key string, plaintext string, iv []byte, additionalData []byte) (string, error) {
@@ -37,4 +43,33 @@ func GCM_decrypt(key string, ct string, iv string, additionalData []byte) (strin
 	}
 	s := string(plaintext[:])
 	return s, nil
+}
+
+func generateSalt(n int) ([]byte, error) {
+	b := make([]byte, n)
+	_, err := rand.Read(b)
+	return b, err
+}
+
+func GenerateFromPassword(password string) string {
+
+	salt, _ := generateSalt(8)
+	nHash := pbkdf2.Key([]byte(password), salt, 4096, sha256.Size, sha256.New)
+	stringed := hex.EncodeToString(salt) + "-" + hex.EncodeToString(nHash)
+
+	return stringed
+}
+
+func CompareHashAndPassword(hashedPassword string, password string) error {
+
+	token := strings.Split(hashedPassword, "-")
+	salt, _ := hex.DecodeString(token[0])
+	sHash, _ := hex.DecodeString(token[1])
+	rHash := pbkdf2.Key([]byte(password), salt, 4096, sha256.Size, sha256.New)
+	if bytes.Equal(sHash, rHash) {
+		return nil
+	}
+
+	return errors.New("invalid password")
+
 }
